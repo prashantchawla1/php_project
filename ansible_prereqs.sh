bd54bb9524bec3ee38ed072ee3fa6a859376aa6e
Prashant Chawla <prashant.a.chawla1@gmail.com>
2019-10-15 08:23:56 +0000

    second commit

---
+++ ansible_prereqs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update -y
+sudo apt-get install ansible -y
+sudo apt-get install python3 -y
+sudo apt-get install apache2 -y
+sudo apt-get install php -y
+sudo apt install unzip
